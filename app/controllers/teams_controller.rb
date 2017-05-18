9372aea1f954e61e76cf12dc4564b1d2e277e6dc
redhendery <redhendery@gmail.com>
2017-05-18 10:23:22 -0700

    refactor teams controller, removed un-used methods, refactor partials & views accordingly

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,17 +5,8 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
-    @sm_logo = @team.sm_logo
-    @lg_logo = @team.lg_logo
-
     # Skater lookup
     @skate = @team.players.where(posAbr: %w[F D])
-
-    # Player stats lookup
-    @points = @skate.limit(5).order(points: :desc)
-    @goals = @skate.limit(5).order(goals: :desc)
-    @assists = @skate.limit(5).order(assists: :desc)
-    @svs = @team.players.where(posAbr: 'GK').limit(5).order(svs: :desc)
   end
 
 end
