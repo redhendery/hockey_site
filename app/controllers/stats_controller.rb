456bed09c81274ee8c87c3526e53d57bce0ac901
redhendery <redhendery@gmail.com>
2017-05-23 08:11:24 -0700

    merge

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,8 @@
+class StatsController < ApplicationController
+  def index
+    @skate = Player.all.where(posAbr: %w[F D])
+    @gk = Player.all.where.not(posAbr: %w[F D], gp: '0')
+  end
+  def show
+  end
+end
