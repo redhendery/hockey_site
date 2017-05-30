32fcc4e8a0e04b92a099bc683754b01a66c0ee6a
redhendery <redhendery@gmail.com>
2017-05-30 10:01:34 -0700

    add lookup to stats by team, add partial to do same by position

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,10 @@
 class StatsController < ApplicationController
   def index
+    @teams = Team.where(nil)
     @skate = Player.all.where(posAbr: %w[F D])
     @gk = Player.all.where.not(posAbr: %w[F D], gp: '0')
+    @gk = @gk.where(team_id: params[:team_id]) if params[:team_id].present?
+    @skate = @skate.where(team_id: params[:team_id]) if params[:team_id].present?
   end
   def show
   end
