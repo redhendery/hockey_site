c13d79be783c2f532a1a286e21f37b565ca76f93
redhendery <redhendery@gmail.com>
2017-06-01 08:54:22 -0700

    add GK table and center text in columns

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -63,3 +63,19 @@ $(document).on("turbolinks:load", function() {
     });
   }).draw();
 });
+
+$(document).on("turbolinks:load", function() {
+  var t;
+  t = $('#gk').DataTable({
+    'pageLength': 25,
+    'order': [[6, "asc"]]
+  });
+  t.on('order.dt search.dt', function() {
+    t.column(0, {
+      search: 'applied',
+      order: 'applied'
+    }).nodes().each(function(cell, i) {
+      cell.innerHTML = i + 1;
+    });
+  }).draw();
+});
