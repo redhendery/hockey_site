a961d62a218b732a69b973b7d136d735639713f4
redhendery <redhendery@gmail.com>
2017-06-07 11:11:25 -0700

    add activeadmin gem for basic standings & stat management

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :teams, only: [:index, :show] do
     resources :players, only: [:index, :show]
     resources :stats, only: [:index] do
