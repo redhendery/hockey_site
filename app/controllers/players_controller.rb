153cd2b7723340733a29b76a5f0dc7c63bbe6a8e
redhendery <redhendery@gmail.com>
2017-06-11 17:39:11 -0700

    clean up a bunch of code with rubocop, add eager loading to schedules_controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,17 +1,21 @@
 class PlayersController < ApplicationController
   before_action :teams
+
   def index
     @gk = Player.where(posAbr: 'GK')
     @skaters = Player.where(posAbr: %w[D F]).order(:posAbr)
     @skaters = @skaters.where(team_id: params[:team_id]) if params[:team_id].present?
     @gk = @gk.where(team_id: params[:team_id]) if params[:team_id].present?
   end
+
   def show
     @player = Player.find(params[:id])
   end
+
   def all
     @players = Player.all
   end
+
   private
   def teams
     @teams = Team.where(nil)
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,31 +1,46 @@
 class SchedulesController < ApplicationController
   before_action :schedules, :teams
+
   def index
+
   end
+
   def show
     @schedule = Schedule.find(params[:id])
     @away = @schedule.away_team
     @home = @schedule.home_team
   end
+
   def swarm
-    @swarm = @schedules.where(home_team_id: 1).or(@schedules.where(away_team_id: 1)).map
+    @swarm = @schedules.where(home_team_id: 1)
+    .or(@schedules.where(away_team_id: 1)).map
   end
+
   def reddevils
-    @devils = @schedules.where(home_team_id: 2).or(@schedules.where(away_team_id: 2)).map
+    @devils = @schedules.where(home_team_id: 2)
+    .or(@schedules.where(away_team_id: 2)).map
   end
+
   def thunder
-    @thunder = @schedules.where(home_team_id: 3).or(@schedules.where(away_team_id: 3)).map
+    @thunder = @schedules.where(home_team_id: 3)
+    .or(@schedules.where(away_team_id: 3)).map
   end
+
   def stampede
-    @stampede = @schedules.where(home_team_id: 4).or(@schedules.where(away_team_id: 4)).map
+    @stampede = @schedules.where(home_team_id: 4)
+    .or(@schedules.where(away_team_id: 4)).map
   end
+
   def admirals
-    @admirals = @schedules.where(home_team_id: 5).or(@schedules.where(away_team_id: 5)).map
+    @admirals = @schedules.where(home_team_id: 5)
+    .or(@schedules.where(away_team_id: 5)).map
   end
+
   private
   def schedules
-    @schedules = Schedule.where(nil)
+    @schedules = Schedule.where(nil).includes(%i[home_team away_team])
   end
+
   def teams
     @teams = Team.where(nil)
   end
