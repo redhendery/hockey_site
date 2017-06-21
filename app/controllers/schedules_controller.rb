61564a4d22ed796ad9e3b76118f7f71be47be5d3
redhendery <redhendery@gmail.com>
2017-06-21 14:10:23 -0700

    modify next method to show games upcoming and completed

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,5 @@
 class SchedulesController < ApplicationController
-  before_action :schedules, :teams
+  before_action :schedules
 
   def index
   end
@@ -11,46 +11,43 @@ class SchedulesController < ApplicationController
   end
 
   def next
-    @next_round = @schedules.where(completed: false).limit(4)
-  end
-
-  def scores
-    @completed = @schedules.where(completed: true)
+    if Date.today.tuesday? || Date.today.wednesday? || Date.today.thursday?
+      @schedules = @schedules.where(completed: :false).limit(4)
+    else
+      @schedules = @schedules.where('date >= ?', Date.today)
+        .order(date: :asc, leagueGame: :asc).limit(4)
+    end
   end
 
   def swarm
     @swarm = @schedules.where(home_team_id: 1)
-      .or(@schedules.where(away_team_id: 1)).map
+      .or(@schedules.where(away_team_id: 1))
   end
 
   def reddevils
     @devils = @schedules.where(home_team_id: 2)
-      .or(@schedules.where(away_team_id: 2)).map
+      .or(@schedules.where(away_team_id: 2))
   end
 
   def thunder
     @thunder = @schedules.where(home_team_id: 3)
-      .or(@schedules.where(away_team_id: 3)).map
+      .or(@schedules.where(away_team_id: 3))
   end
 
   def stampede
     @stampede = @schedules.where(home_team_id: 4)
-      .or(@schedules.where(away_team_id: 4)).map
+      .or(@schedules.where(away_team_id: 4))
   end
 
   def admirals
     @admirals = @schedules.where(home_team_id: 5)
-      .or(@schedules.where(away_team_id: 5)).map
+      .or(@schedules.where(away_team_id: 5))
   end
 
   private
 
     def schedules
       @schedules = Schedule.where(nil).includes(%i[home_team away_team])
-    end
-
-    def teams
-      @teams = Team.where(nil)
     end
 
 end
