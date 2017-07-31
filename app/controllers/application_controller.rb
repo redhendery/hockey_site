5c2e61982be4fad66a32a4f192478b6b16cfe3ed
redhendery <redhendery@gmail.com>
2017-07-31 16:50:27 -0700

    remove helpers that arent there so app works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include AdmiralsHelper, DevilsHelper, GameStatHelper, SessionsHelper,
-    StampedeHelper, SwarmHelper, SchedulesHelper, TeamsHelper, ThunderHelper
+  include GameStatHelper, SessionsHelper, SchedulesHelper, TeamsHelper
 end
